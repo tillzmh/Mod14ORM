ab163954464117346f7382b931bb61e8672aea53
tillzmh <tillzmh@users.noreply.github.com>
2022-09-25 18:05:15 -0400

    which this the store will now to tell what product belongs to what category, how many products are in a setion and etc

--- fantastic-umbrella-main/Develop/models/index.js
+++ fantastic-umbrella-main/Develop/models/index.js
@@ -5,13 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category',
+});
 
 // Categories have many Products
-
+Category.hasMany(Products,{
+  foreignKey: 'category',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tags, {
+  through: 'ProductTag',
+  foreignKey: 'product',
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Products, {  
+  throw: 'ProductTag',
+  foreignKey: 'tag',
+});
 module.exports = {
   Product,
   Category,
