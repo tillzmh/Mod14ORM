6b6cdc34c1415691ac8f5dbf23944f2a6896b1d0
tillzmh <tillzmh@users.noreply.github.com>
2022-09-25 10:28:26 -0400

    defined types of colums

--- fantastic-umbrella-main/Develop/models/Category.js
+++ fantastic-umbrella-main/Develop/models/Category.js
@@ -6,9 +6,18 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
+    catagory_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+    },
+    {
     sequelize,
     timestamps: false,
     freezeTableName: true,
