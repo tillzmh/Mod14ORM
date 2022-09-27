2add94ea8d71032a5e17992d1acf320267e73bca
tillzmh <tillzmh@users.noreply.github.com>
2022-09-27 15:22:57 -0400

    now youll be able to update category by ID. if not found respond with a error messages stating can not be found

--- fantastic-umbrella-main/Develop/routes/api/category-routes.js
+++ fantastic-umbrella-main/Develop/routes/api/category-routes.js
@@ -60,7 +60,22 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCatData => {
+      if (!dbCatData) {
+        res.status(404).json({message:'category not found with this id'});
+        return;
+      }
+      res.json(dbCatData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
