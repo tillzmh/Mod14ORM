b95ab34cee6a69e67c620818852e857430528e67
tillzmh <tillzmh@users.noreply.github.com>
2022-09-27 22:49:34 -0400

    destroy method used again that will be able to delete a tag

--- fantastic-umbrella-main/Develop/routes/api/tag-routes.js
+++ fantastic-umbrella-main/Develop/routes/api/tag-routes.js
@@ -14,6 +14,12 @@ router.get('/:id', (req, res) => {
       model: Product,
       attributes: ['product_name', 'price', 'stock', 'category_id']
     }
+})
+.then(dbTagData => res.json(dbTagData))
+.catch(err => {
+  console.log(err);
+  res.status(500).json(err);
+});
 });
 
 router.post('/', (req, res) => {
@@ -47,7 +53,22 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({message: 'tag not found'});
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
