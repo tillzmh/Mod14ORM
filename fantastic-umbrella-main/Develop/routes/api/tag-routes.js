a07eca6e8e7bddb78b3c499644ee9cc0a1ac7753
tillzmh <tillzmh@users.noreply.github.com>
2022-09-27 22:46:20 -0400

    update function able to will update tags name by id

--- fantastic-umbrella-main/Develop/routes/api/tag-routes.js
+++ fantastic-umbrella-main/Develop/routes/api/tag-routes.js
@@ -28,7 +28,22 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData){
+        res.status(404).json({message:'tag not found'});
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
