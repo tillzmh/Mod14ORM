c950a7cade34518844cb31c3a4651b0be8661d64
tillzmh <tillzmh@users.noreply.github.com>
2022-10-02 23:16:07 -0400

    now produts are able to be deleted by its id

--- fantastic-umbrella-main/Develop/routes/api/product-routes.js
+++ fantastic-umbrella-main/Develop/routes/api/product-routes.js
@@ -66,6 +66,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -132,6 +133,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+      rs.status(404).json({message: 'product not found with this id'});
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
